f7b9675534811062d92551110193669d416d9cb2
Jorge Maldonado Borbón <jorge@2806-108e-0005-6bc8-10d2-38db-4b6d-6fc2.ipv6.infinitum.net.mx>
2018-01-09 20:18:38 -0600

    Initial Commit

---
+++ NotificacionLocal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotificacionLocal
+//
+//  Created by Jorge Maldonado Borbón on 09/01/18.
+//  Copyright © 2018 Jorge Maldonado Borbón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
